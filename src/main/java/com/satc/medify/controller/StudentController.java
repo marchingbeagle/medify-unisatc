e5166d135a6a4152c15e7db7c555c2149d1f5f04
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-06-10 20:40:04 -0300

    route ajuda added

---
+++ src/main/java/com/satc/medify/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.satc.medify.controller;
+
+import com.satc.medify.model.Student;
+import com.satc.medify.model.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping()
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    private final String PROJECT_NAME = "Medify";
+    private final String PROJECT_THEME = "Saúde e Bem-estar: Gerenciamento de medicamentos, mapeamento de atendimentos médicos, recomendações médicas;";
+
+    @GetMapping("/ajuda")
+    public Object getHelp() {
+        List<String> students = studentRepository.findAll().stream()
+                .map(Student::getName)
+                .collect(Collectors.toList());
+
+        return new Object() {
+            public List<String> estudantes = students;
+            public String projeto = PROJECT_NAME;
+            public String tema = PROJECT_THEME;
+        };
+    }
+}
---
+++ src/main/resources/db/migration/V3__insertStudents.sql
@@ -0,0 +1,3 @@
+INSERT INTO student (name) VALUES ('Erik Schneider Pacheco');
+INSERT INTO student (name) VALUES ('Dauane Neves Gerrônimo');
+INSERT INTO student (name) VALUES ('Gabriel Willian Bernardino Duarte');

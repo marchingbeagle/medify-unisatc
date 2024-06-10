5c10d6857822da7ab4dec4824ab2cd1bd50ef818
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-06-10 17:55:00 -0300

    initial files

---
+++ src/test/java/com/satc/medify/MedifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satc.medify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

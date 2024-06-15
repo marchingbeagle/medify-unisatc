6367546dfb1cefc8092d5dafae8a29095af8be21
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-06-15 15:49:37 -0300

    dockerfile fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:17-jdk-slim
 VOLUME /tmp
 COPY target/medify-0.0.1-SNAPSHOT.jar app.jar
-COPY src/main/resources/.env .env
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/satc/medify/MedifyApplication.java
+++ src/main/java/com/satc/medify/MedifyApplication.java
@@ -1,6 +1,5 @@
 package com.satc.medify;
 
-import com.satc.medify.service.DotenvLoadService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedifyApplication {
 
 	public static void main(String[] args) {
-		//DotenvLoadService.load("./src/main/resources/.env");
 		SpringApplication.run(MedifyApplication.class, args);
 	}
 
